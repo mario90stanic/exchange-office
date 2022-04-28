8413d7e5e6ca881d0f3a1cc81cf2db22cc3fe971
Mario Stanic <mario@forwardslashny.com>
2022-04-28 18:44:47 +0200

    inserting order data into database

--- app/Http/Controllers/ExchangeController.php
+++ app/Http/Controllers/ExchangeController.php
@@ -1,7 +1,12 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\FetchingCurrenciesFailed;
+use App\Factories\CurrencyFactory;
+use App\Http\Requests\OrderRequest;
+use App\Models\Value;
+use Exception;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
@@ -9,8 +14,92 @@ use Illuminate\Http\Response;
 
 class ExchangeController extends Controller
 {
+    /**
+     * @throws Exception
+     */
     public function getValues(): Response|Application|ResponseFactory
     {
-        return response(['123', 455], 200);
+        try {
+            $currency = Value::getValueNames();
+        } catch (FetchingCurrenciesFailed $e) {
+            return response([
+                'status' => 'no_currencies_in_database',
+                'message' => 'There are no currencies in the system.',
+            ], 500);
+        } catch (\Exception $e) {
+            return response([
+                'status' => 'returning_currencies_failed',
+                'message' => 'Fetching currencies failed.',
+            ], 500);
+        }
+
+        return response([
+            'status' => 'returning_currencies',
+            'message' => 'Fetching currencies successfully.',
+            'currencies' => $currency,
+        ], 200);
+    }
+
+    public function makeExchangeOrder(OrderRequest $order): Response|Application|ResponseFactory
+    {
+        try {
+            $order = $order->validated();
+            $currency = CurrencyFactory::make($order['amount'], $order['currency']);
+            $currency->setUp();
+            $order = $currency->makeOrder();
+        } catch (\Exception $e) {
+            return response([
+                'status' => 'order_creat_failed',
+                'message' => 'Creating of the order failed.',
+            ], 500);
+        }
+
+        return response([
+            'status' => 'order_successfully_created',
+            'message' => 'Order is created successfully.',
+            'order' => $order,
+        ], 200);
+    }
+
+    public function getCalculateWithSurcharge(OrderRequest $order): Response|Application|ResponseFactory
+    {
+        try {
+            $order = $order->validated();
+            $currency = CurrencyFactory::make($order['amount'], $order['currency']);
+            $currency->setUp();
+            $calculated = $currency->getCalculatedWithSurcharge();
+        } catch (\Exception $e){
+            return response([
+                'status' => 'calculating_amount_with_surcharge_failed',
+                'message' => 'Calculating of the amount with surcharge failed.',
+            ], 500);
+        }
+
+        return response([
+            'status' => 'calculated_amount_with_surcharge',
+            'message' => 'Calculated amount without surcharge.',
+            'amount' => $calculated,
+        ], 200);
+    }
+
+    public function getCalculate(OrderRequest $order): Response|Application|ResponseFactory
+    {
+        try {
+            $order = $order->validated();
+            $currency = CurrencyFactory::make($order['amount'], $order['currency']);
+            $currency->setUp();
+            $calculated =  $currency->getCalculatedAmount();
+        } catch (\Exception $e) {
+            return response([
+                'status' => 'calculating_amount_failed',
+                'message' => 'Calculating of the amount failed.',
+            ], 500);
+        }
+
+        return response([
+            'status' => 'calculated_amount',
+            'message' => 'Calculated amount without surcharge.',
+            'amount' => $calculated,
+        ], 200);
     }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1,13 +1,21 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Currencies\Currency;
+use App\Exceptions\FetchingCurrenciesFailed;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static insert(array $array)
+ */
 class Order extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
 
     protected $fillable = [
         'foreign_currency_purchased',
@@ -18,8 +26,38 @@ class Order extends Model
         'amount_in_usd',
         'discount_percentage',
         'discount_amount',
-        'created_at',
     ];
+
+    protected $casts = [
+        'foreign_currency_exchange_rate' => 'float',
+        'surcharge_percentage' => 'float',
+        'surcharge_amount' => 'float',
+        'foreign_currency_amount' => 'float',
+        'amount_in_usd' => 'float',
+        'discount_percentage' => 'float',
+        'discount_amount' => 'float',
+    ];
+
+    /**
+     * @throws FetchingCurrenciesFailed
+     */
+    public static function make(Currency $currency)
+    {
+        //todo: create method for calculating discount and surcharge
+        $order = Order::create([
+            'foreign_currency_purchased' => $currency->getCurrency()->name,
+            'foreign_currency_exchange_rate' => $currency->getCurrency()->rate,
+            'surcharge_percentage' => $currency->getCurrency()->surcharge,
+            'surcharge_amount' => $currency->getAmount() * $currency->getCurrency()->surcharge / 100,
+            'foreign_currency_amount' => $currency->getAmount(),
+            'amount_in_usd' => $currency->getCalculatedWithSurcharge(),
+            'discount_percentage' => $currency->getCurrency()->discount,
+            'discount_amount' => $currency->getCalculatedAmount() * $currency->getCurrency()->discount / 100,
+            'created_at' => Carbon::now()
+        ]);
+
+       return $order;
+    }
 
     public static function boot()
     {
